0de66d8f2a41c758781efd725d858b898dec1483
AparnaTerzo <aparna@terzocloud.com>
2023-06-16 12:51:55 +0530

    first commit

--- src/Demo.java
+++ src/Demo.java
@@ -2,10 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Demo {
     public static void main(String[] args){
-        greet(new Lion());
+        greet(new Animal() {
+            @Override
+            public void display(String message) {
+                System.out.println(message);
+            }
+        });
     }
 
     public static void greet(Animal animal){
+
         System.out.println("It is a lion");
     }
     }
